aa51d9526ac832042a94dc7f5428a77f3aa443d2
michalpaprocki <michalpaprocki@proton.me>
2025-12-04 14:31:01 +0100

    initial commit

---
+++ src/main/java/com/mike/taskmaster/controllers/mainController.java
@@ -0,0 +1,27 @@
+package com.mike.taskmaster.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class mainController {
+    
+    @GetMapping("/")
+    public String home() {
+        return """
+                <!DOCTYPE html>
+                <html lang="en>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>TaskMaster</title>
+                </head>
+                <body>
+                    <h1>Welcome to TaskMaster API</h1>
+                    <p>This is the main page.</p>
+                </body>
+                </html>
+                """;
+    }
+    
+}
