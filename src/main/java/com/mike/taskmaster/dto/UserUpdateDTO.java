988cd7d410a6717a911e8df3bc871fef153a3509
michalpaprocki <michalpaprocki@proton.me>
2025-12-10 11:35:53 +0100

    create userupdatedto

---
+++ src/main/java/com/mike/taskmaster/dto/UserUpdateDTO.java
@@ -0,0 +1,58 @@
+package com.mike.taskmaster.dto;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+import static com.mike.taskmaster.dto.UserRequestDTO.EMAIL_INVALID_MSG;
+import static com.mike.taskmaster.dto.UserRequestDTO.PASSWORD_INVALID_MSG;
+
+public class UserUpdateDTO {
+    private UUID id;
+    private String name;
+
+    @Email(message = EMAIL_INVALID_MSG)
+    private String email;
+
+
+    @Pattern(
+    regexp = "^(?=.*[a-z])(?=.*[A-Z]).{6,}$",
+    message = PASSWORD_INVALID_MSG
+    )
+    private String password;
+
+    public UserUpdateDTO() {}
+
+    public UserUpdateDTO(UUID id) {
+        this.id = id;
+    }
+
+    public UserUpdateDTO(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {return id;}
+    public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
+    
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
