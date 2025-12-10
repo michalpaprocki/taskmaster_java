d72ed4f0048464292a90e5aa03f5cf2dbb2034ff
michalpaprocki <michalpaprocki@proton.me>
2025-12-10 11:37:15 +0100

    refactored userservice along with tests

--- src/main/java/com/mike/taskmaster/service/UserService.java
+++ src/main/java/com/mike/taskmaster/service/UserService.java
@@ -1,26 +1,32 @@
 package com.mike.taskmaster.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import com.mike.taskmaster.repository.UserRepository;
-import com.mike.taskmaster.dto.UserDTO;
+import com.mike.taskmaster.dto.UserRequestDTO;
+import com.mike.taskmaster.dto.UserUpdateDTO;
 import com.mike.taskmaster.entity.User;
 @Service
 public class UserService {
     
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
-    public User createUser(UserDTO userDto) {
+    public User createUser(UserRequestDTO userDto) {
         if (userRepository.existsByEmail(userDto.getEmail())) {
             throw new IllegalArgumentException("Email already taken");
         }
 
-        User user = new User(userDto.getName(), userDto.getEmail());
+        User user = new User(userDto.getName(), userDto.getEmail(), userDto.getPassword());
         return userRepository.save(user);
     }
     public User getUser(UUID id) {
@@ -32,4 +38,32 @@ public class UserService {
         User user = getUser(id);
         return user.getEmail();
     }
+
+    public User updateUser(UserUpdateDTO dto) {
+        User user = userRepository.findById(dto.getId()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        if (dto.getName() != null) 
+            user.setName(dto.getName());
+        if (dto.getEmail() != null)
+            user.setEmail(dto.getEmail());
+        if (dto.getPassword() != null)
+            user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public String softDeleteUser(UserUpdateDTO dto) {
+        User user = userRepository.findById(dto.getId()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.setIsDeleted(true);
+        user.setDeletedAt(LocalDateTime.now());
+        userRepository.save(user);
+        return "User deleted successfully";
+    }
+
+    public String hardDeleteUser(UserUpdateDTO dto) {
+        User user = userRepository.findById(dto.getId()).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        userRepository.delete(user);
+        return "User deleted successfully";
+    }
+    
+    
+
 }
--- src/test/java/com/mike/taskmaster/service/UserServiceTest.java
+++ src/test/java/com/mike/taskmaster/service/UserServiceTest.java
@@ -5,17 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 import com.mike.taskmaster.repository.UserRepository;
-import com.mike.taskmaster.dto.UserDTO;
+import com.mike.taskmaster.dto.UserRequestDTO;
+import com.mike.taskmaster.dto.UserUpdateDTO;
 import com.mike.taskmaster.entity.User;
 
 
@@ -31,13 +38,13 @@ public class UserServiceTest {
 
     @Autowired
     private UserRepository userRepository;
-
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private User jane;
 
     @BeforeEach
     void cleanAndSetupDb(){
 
-        UserDTO janeDTO = new UserDTO("jane", "jane@example.com");
+        UserRequestDTO janeDTO = new UserRequestDTO("jane", "jane@example.com", "Amazing");
 
         
         this.jane = userService.createUser(janeDTO);
@@ -45,7 +52,7 @@ public class UserServiceTest {
 
     @Test
     void testCreateUser() {
-        UserDTO dto = new UserDTO("tony", "tony@example.com");
+        UserRequestDTO dto = new UserRequestDTO("tony", "tony@example.com", "Amazing");
         User user = userService.createUser(dto);
         assertThat(user).isNotNull();
         assertThat(user.getName()).isEqualTo("tony");
@@ -55,7 +62,7 @@ public class UserServiceTest {
 
     @Test
     void testDuplicateEmailThrows() {
-        UserDTO dto = new UserDTO("tony", jane.getEmail());
+        UserRequestDTO dto = new UserRequestDTO("tony", jane.getEmail(), jane.getPassword());
         assertThatThrownBy(()-> userService.createUser(dto)).isInstanceOf(IllegalArgumentException.class).hasMessage("Email already taken");
     }
 
@@ -78,5 +85,32 @@ public class UserServiceTest {
         String email = userService.getEmail(jane.getId());
         assertThat(email).isNotNull();
         assertThat(email).isEqualTo(jane.getEmail());
+    }
+
+    @Test
+    void testUserUpdate() {
+        UserUpdateDTO dto = new UserUpdateDTO(jane.getId(), "cassandra", "cassandra@example.com", "my_secr3t_Passwordz");
+        User user = userService.updateUser(dto);
+        assertThat(user.getName()).isEqualTo("cassandra");
+        assertThat(user.getEmail()).isEqualTo("cassandra@example.com");
+        assertNotEquals(dto.getPassword(), user.getPassword());
+        assertTrue(passwordEncoder.matches(dto.getPassword(), user.getPassword()));
+    }
+
+    @Test
+    void testUserSoftDelete() {
+        UserUpdateDTO dto = new UserUpdateDTO(jane.getId());
+        String responseString = userService.softDeleteUser(dto);
+        assertThat(responseString).isEqualTo("User deleted successfully");
+        User user = userService.getUser(dto.getId());
+        assertThat(user).isNotNull();
+        assertThat(user.getIsDeleted()).isEqualTo(true);
+        assertThat(user.getDeletedAt()).isInstanceOf(LocalDateTime.class);
+    }
+    void testUserHardDelete() {
+        UserUpdateDTO dto = new UserUpdateDTO(jane.getId());
+        String responseString = userService.hardDeleteUser(dto);
+        assertThat(responseString).isEqualTo("User deleted successfully");
+        assertThatThrownBy(() -> userService.getUser(dto.getId())).isInstanceOf(IllegalArgumentException.class).hasMessage("USer not found");
     }
 }
