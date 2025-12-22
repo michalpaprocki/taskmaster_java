feef57ddae5c1017d73bbff7fe09dd69e9625e68
michalpaprocki <michalpaprocki@proton.me>
2025-12-22 10:12:53 +0100

    remove unnecessary config that crashed the app from TaskmasterApplication

--- src/main/java/com/mike/taskmaster/TaskmasterApplication.java
+++ src/main/java/com/mike/taskmaster/TaskmasterApplication.java
@@ -1,9 +1,5 @@
 package com.mike.taskmaster;
 
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,11 +8,6 @@ public class TaskmasterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmasterApplication.class, args);
-	}
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(crsf -> crsf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		return http.build();
 	}
 
 }
