b1356aae75b633ff01082ca89718fd13d2d8661c
michalpaprocki <michalpaprocki@proton.me>
2026-01-27 14:11:21 +0100

    added find by email method to user repo

--- src/main/java/com/mike/taskmaster/repository/UserRepository.java
+++ src/main/java/com/mike/taskmaster/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.mike.taskmaster.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import com.mike.taskmaster.entity.User;
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     boolean existsByName(String name);
+    Optional<User> findByEmail(String email);
 }
