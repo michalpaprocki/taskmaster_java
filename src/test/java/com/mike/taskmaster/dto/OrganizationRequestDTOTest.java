f30ed472aeff62033b3ddcc764b3ddf54fb93649
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 09:30:11 +0100

    updated dto test

--- src/test/java/com/mike/taskmaster/dto/OrganizationRequestDTOTest.java
+++ src/test/java/com/mike/taskmaster/dto/OrganizationRequestDTOTest.java
@@ -2,7 +2,10 @@ package com.mike.taskmaster.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +15,13 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
+import static com.mike.taskmaster.dto.OrganizationRequestDTO.DESC_MIN_MSG;
 import static com.mike.taskmaster.dto.OrganizationRequestDTO.NAME_MIN_MSG;
 import static com.mike.taskmaster.dto.OrganizationRequestDTO.NAME_REQUIRED_MSG;;
 
 public class OrganizationRequestDTOTest {
     private Validator validator;
-
+    private List<UUID> uuids = new ArrayList<>();
     @BeforeEach
     void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
@@ -26,7 +30,7 @@ public class OrganizationRequestDTOTest {
 
     @Test
     void testNameMinLength() {
-        OrganizationRequestDTO dto = new OrganizationRequestDTO("Wa", OrganizationRequestDTO.Action.ADD);
+        OrganizationRequestDTO dto = new OrganizationRequestDTO("Wa", null, uuids, OrganizationRequestDTO.Action.ADD);
         Set<ConstraintViolation<OrganizationRequestDTO>> violations = validator.validate(dto);
         assertThat(violations).anyMatch(v -> 
             v.getPropertyPath().toString().equals("name") && 
@@ -35,7 +39,7 @@ public class OrganizationRequestDTOTest {
 
     @Test
     void testNameNotBlank() {
-        OrganizationRequestDTO dto = new OrganizationRequestDTO("", OrganizationRequestDTO.Action.ADD);
+        OrganizationRequestDTO dto = new OrganizationRequestDTO("", "", uuids, OrganizationRequestDTO.Action.ADD);
         Set<ConstraintViolation<OrganizationRequestDTO>> violations = validator.validate(dto);
         assertThat(violations).anyMatch(v -> 
             v.getPropertyPath().toString().equals("name") && 
@@ -44,8 +48,17 @@ public class OrganizationRequestDTOTest {
 
     @Test
     void testValidDTO() {
-        OrganizationRequestDTO dto = new OrganizationRequestDTO("Wayland", OrganizationRequestDTO.Action.ADD);
+        OrganizationRequestDTO dto = new OrganizationRequestDTO("Wayland", "my organization description", uuids, OrganizationRequestDTO.Action.ADD);
         Set<ConstraintViolation<OrganizationRequestDTO>> violations = validator.validate(dto);
         assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void testDescriptionMinLength() {
+        OrganizationRequestDTO dto = new OrganizationRequestDTO("Wayland", "my", uuids, OrganizationRequestDTO.Action.ADD);
+        Set<ConstraintViolation<OrganizationRequestDTO>> violations = validator.validate(dto);
+        assertThat(violations).anyMatch(v ->
+            v.getPropertyPath().toString().equals("description") && 
+            v.getMessage().equals(DESC_MIN_MSG));
     }
 }
