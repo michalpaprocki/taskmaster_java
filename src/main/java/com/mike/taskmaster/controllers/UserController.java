bb2776e70c6d72e34e775187515559f9fa5fa3cd
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 15:32:07 +0100

    added change password endpoint

--- src/main/java/com/mike/taskmaster/controllers/UserController.java
+++ src/main/java/com/mike/taskmaster/controllers/UserController.java
@@ -3,17 +3,24 @@ package com.mike.taskmaster.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.mike.taskmaster.dto.UserResponseDTO;
+import com.mike.taskmaster.dto.ChangePasswordRequestDTO;
 import com.mike.taskmaster.exception.JwtValidationException;
 import com.mike.taskmaster.security.jwt.JwtTokenProvider;
 import com.mike.taskmaster.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+
 
 
 @RestController
@@ -43,6 +50,18 @@ public class UserController {
 
             return ResponseEntity.ok(user);
         }
+
+    @Operation(summary = "Changes user password")
+    @PatchMapping("/me/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(@RequestBody ChangePasswordRequestDTO dto, @CookieValue(name = "accessToken", required = false) String token){
+          if (token == null) {
+            throw new JwtValidationException("Missing jwt token");
+        }
+            UUID id = jwtTokenProvider.parseTokenForUUID(token);
+
+            userService.changePassword(id, dto);
+    }
 
     @Operation(summary = "Returns all the users")
     @GetMapping
